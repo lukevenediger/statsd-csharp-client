7fb45d1b61501b6d699b155a36a0e1dbeb487c27
Blake Niemyjski <bniemyjski@gmail.com>
2017-01-05 08:53:05 -0600

    Added pdb info for gitlink

--- StatsdClient/OutputChannelExtensions.cs
+++ StatsdClient/OutputChannelExtensions.cs
@@ -2,6 +2,11 @@
 {
     public static class OutputChannelExtensions
     {
+        /// <summary>
+        ///
+        /// </summary>
+        /// <param name="outputChannel"></param>
+        /// <param name="line"></param>
         public static void Send(this IOutputChannel outputChannel, string line)
         {
             outputChannel.SendAsync(line).ConfigureAwait(false).GetAwaiter().GetResult();
